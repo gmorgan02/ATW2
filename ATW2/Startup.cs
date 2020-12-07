c6fc86d3adee18dc8fb643bc7e4b4c734b951b69
George Morgan <georgecranmermorgan@outlook.com>
2020-12-07 11:32:48 +0000

    Initial view changes

--- ATW2/Startup.cs
+++ ATW2/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 
 namespace ATW2
 {
@@ -24,6 +20,7 @@ namespace ATW2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
